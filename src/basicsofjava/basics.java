fced6173adb3a60917267f8f5c455a0921c5126c
yv24 <yash.varshney24@gmail.com>
2024-02-20 01:12:42 +0530

    modified

--- src/basicsofjava/basics.java
+++ src/basicsofjava/basics.java
@@ -3,6 +3,6 @@ package basicsofjava;
 public class basics {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.print("hello");
+       
     }
 }
